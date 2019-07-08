713d9e8d576722545bd9933940e6a0692f8e0e51
brian hensley and mathew akre <brian+mathew@>
2019-07-08 10:55:47 -0700

    add weekend homework

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,37 +1,38 @@
 import React from 'react';
-import TicketList from './TicketList';
 import Header from './Header';
+import TicketList from './TicketList';
+import NewTicketControl from './NewTicketControl';
+import Error404 from './Error404';
 import { Switch, Route } from 'react-router-dom';
-import NewTicketForm from './NewTicketForm.jsx';
 
-function App() {
+class App extends React.Component {
 
-  return (
-    <div>
-      <style global jsx>{`
-        body {
-          background-color: #3D3A39;
-        }
-        body::after {
-          content: "";
-          background: url("https://static1.squarespace.com/static/5524448ee4b0d6f6b83ab9e2/t/56f32109859fd0cad67fe1e1/1458774289324");
-          background-size: cover;
-          opacity: 0.2;
-          top: 0;
-          left: 0;
-          bottom: 0;
-          right: 0;
-          position: absolute;
-          z-index: -1;   
-        }
-      `}</style>
-      <Header />
-      <Switch>
-        <Route exact path='/' component={TicketList} />
-        <Route path='/newticket' component={NewTicketForm} />
-      </Switch>
-    </div>
-  );
+  constructor(props) {
+    super(props);
+    this.state = {
+      masterTicketList: []
+    };
+    this.handleAddingNewTicketToList = this.handleAddingNewTicketToList.bind(this);
+  }
+
+  handleAddingNewTicketToList(newticket) {
+    let newMasterTicketList = this.state.masterTicketList.slice();
+    newMasterTicketList.push(newticket);
+    this.setState({masterTicketList: newMasterTicketList});
+  }
+
+  render() {
+    return (
+      <div>
+        <Header />
+        <Switch>
+          <Route exact path='/' render={()=><TicketList ticketList={this.state.masterTicketList} />} />
+          <Route path='/newticket' render={() =><NewTicketControl onNewTicketCreation={this.handleAddingNewTicketToList} />} />
+          <Route component={Error404} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
