a17e77d69b653effd8454c00190019fdf998d57b
Brian Hensley <brnhensley@gmail.com>
2019-07-15 19:28:21 -0700

    add tests

--- _tests_/reducers/ticket-list-reducer.test.js
+++ _tests_/reducers/ticket-list-reducer.test.js
@@ -2,8 +2,38 @@ import ticketListReducer from '../../src/reducers/ticket-list-reducer';
 
 describe('ticketListReducer', () => {
 
+  let action;
+  const sampleTicketData = {
+    names: 'Dick and Dick',
+    locations: 'Uglyville',
+    issue: 'can\'t poop right',
+    timeOpen: 1500000000,
+    id: 0
+  };
+
   test('Should return default state if no action type is recognized', () => {
     expect(ticketListReducer({}, { type: null })).toEqual({});
+  });
+
+  test('Should successfully add new ticket to masterTicketList', () => {
+    const {names, location, issue, timeOpen, id } = sampleTicketData;
+    action = {
+      type: 'ADD_TICKET',
+      names: names,
+      locations: location,
+      issue: issue,
+      timeOpen: timeOpen,
+      id: id
+    };
+    expect(ticketListReducer({}, action)).toEqual({
+      [id] : {
+        names: names,
+        location: location,
+        issue: issue,
+        timeOpen: timeOpen,
+        id: id
+      }
+    });
   });
 
 });
