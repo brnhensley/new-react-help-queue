2572da90c912a3374cada05ec97252cc71e04450
Brian Hensley <brnhensley@gmail.com>
2019-07-17 10:38:36 -0700

    combine reducers

--- _tests_/reducers/selected-ticket-reducer.js
+++
@@ -1,8 +0,0 @@
-import selectedTicketReducer from './../../src/reducers/selected-ticket-reducer'
-
-describe("selectedTicketReducer", () => {
-
-    test('Should return default state is no action type is recognized', () => {
-        expect(selectedTicketReducer({}, { type: null })).toEqual({});
-    });
-});
--- src/reducers/selected-ticket-reducer.js
+++ src/reducers/selected-ticket-reducer.js
@@ -1,3 +1,9 @@
-export default (state= {}, action) => {
+export default (state = {}, action) => {
+  switch (action.type) {
+  case 'SELECT_TICKET':
+    return action.ticketId;
+  default:
     return state;
+
+  }
 };
